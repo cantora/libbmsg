3e1131fbcea0c6bd1d8b2f80e4ab94f1c77d3f73
anthony cantor <tony.cantor@gmail.com>
2013-04-24 18:57:14 -0600

    added some more integer tests
    test that we can reinitialize and reuse an integer parser

--- test/parser_integer_test.c
+++ test/parser_integer_test.c
@@ -19,139 +19,144 @@ int main(int argc, char *argv[]) {
 	uint64_t input;
 	uint8_t bytes[8];
 	size_t i;
+	int j;
 	(void)(argc);
 	(void)(argv);
 
 	srand(time(NULL));
 
-	input = (uint64_t) rand();
-	input |= ((uint64_t) rand()) << 32;
-	
-	bmsg_encode_uint64(bytes, input);
-	plan_tests(3+3+4+4+5+5+5+5+6+6);
+	plan_tests(2*(3+3+4+4+5+5+5+5+6+6));
 
 	diag("++++parser/integer++++\n#");
+
+	for(j = 0; j < 2; j++) {
+		diag("j = %d", j);
+		input = (uint64_t) rand();
+		input |= ((uint64_t) rand()) << 32;
+		bmsg_encode_uint64(bytes, input);
 	
-	bmsg_parser_uint16_init(&u16_prsr);
-	bmsg_parser_uint32_init(&u32_prsr);
-	bmsg_parser_uint64_init(&u64_prsr);
-	ok1(bmsg_parser_uint16_done(&u16_prsr) == 0);
-	ok1(bmsg_parser_uint32_done(&u32_prsr) == 0);
-	ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
-
-	diag("input = 0x%016" PRIx64, input);
-	i = 0;
-	diag("i = %zu", i);
-	bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
-	bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
-	bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
-	ok1(bmsg_parser_uint16_done(&u16_prsr) == 0);
-	ok1(bmsg_parser_uint32_done(&u32_prsr) == 0);
-	ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
-
-	i++;
-	diag("i = %zu", i);
-	bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
-	bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
-	bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
-	ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
-	bmsg_parser_uint16_value(&u16_prsr, &u16_val);
-	ok1(u16_val == bmsg_decode_uint16(bytes));
-	ok1(bmsg_parser_uint32_done(&u32_prsr) == 0);
-	ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
-
-	i++;
-	diag("i = %zu", i);
-	bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
-	bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
-	bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
-	ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
-	bmsg_parser_uint16_value(&u16_prsr, &u16_val);
-	ok1(u16_val == bmsg_decode_uint16(bytes));
-	ok1(bmsg_parser_uint32_done(&u32_prsr) == 0);
-	ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
+		bmsg_parser_uint16_init(&u16_prsr);
+		bmsg_parser_uint32_init(&u32_prsr);
+		bmsg_parser_uint64_init(&u64_prsr);
+		ok1(bmsg_parser_uint16_done(&u16_prsr) == 0);
+		ok1(bmsg_parser_uint32_done(&u32_prsr) == 0);
+		ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
 	
-	i++;
-	diag("i = %zu", i);
-	bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
-	bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
-	bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
-	ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
-	bmsg_parser_uint16_value(&u16_prsr, &u16_val);
-	ok1(u16_val == bmsg_decode_uint16(bytes));
-	ok1(bmsg_parser_uint32_done(&u32_prsr) != 0);
-	bmsg_parser_uint32_value(&u32_prsr, &u32_val);
-	ok1(u32_val == bmsg_decode_uint32(bytes));
-	ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
-
-	i++;
-	diag("i = %zu", i);
-	bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
-	bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
-	bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
-	ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
-	bmsg_parser_uint16_value(&u16_prsr, &u16_val);
-	ok1(u16_val == bmsg_decode_uint16(bytes));
-	ok1(bmsg_parser_uint32_done(&u32_prsr) != 0);
-	bmsg_parser_uint32_value(&u32_prsr, &u32_val);
-	ok1(u32_val == bmsg_decode_uint32(bytes));
-	ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
-
-	i++;
-	diag("i = %zu", i);
-	bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
-	bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
-	bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
-	ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
-	bmsg_parser_uint16_value(&u16_prsr, &u16_val);
-	ok1(u16_val == bmsg_decode_uint16(bytes));
-	ok1(bmsg_parser_uint32_done(&u32_prsr) != 0);
-	bmsg_parser_uint32_value(&u32_prsr, &u32_val);
-	ok1(u32_val == bmsg_decode_uint32(bytes));
-	ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
-
-	i++;
-	diag("i = %zu", i);
-	bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
-	bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
-	bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
-	ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
-	bmsg_parser_uint16_value(&u16_prsr, &u16_val);
-	ok1(u16_val == bmsg_decode_uint16(bytes));
-	ok1(bmsg_parser_uint32_done(&u32_prsr) != 0);
-	bmsg_parser_uint32_value(&u32_prsr, &u32_val);
-	ok1(u32_val == bmsg_decode_uint32(bytes));
-	ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
-
-	i++;
-	diag("i = %zu", i);
-	bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
-	bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
-	bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
-	ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
-	bmsg_parser_uint16_value(&u16_prsr, &u16_val);
-	ok1(u16_val == bmsg_decode_uint16(bytes));
-	ok1(bmsg_parser_uint32_done(&u32_prsr) != 0);
-	bmsg_parser_uint32_value(&u32_prsr, &u32_val);
-	ok1(u32_val == bmsg_decode_uint32(bytes));
-	ok1(bmsg_parser_uint64_done(&u64_prsr) != 0);
-	bmsg_parser_uint64_value(&u64_prsr, &u64_val);
-	ok1(u64_val == bmsg_decode_uint64(bytes));
-
-	i++;
-	diag("i = %zu", i);
-	bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
-	bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
-	bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
-	ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
-	bmsg_parser_uint16_value(&u16_prsr, &u16_val);
-	ok1(u16_val == bmsg_decode_uint16(bytes));
-	ok1(bmsg_parser_uint32_done(&u32_prsr) != 0);
-	bmsg_parser_uint32_value(&u32_prsr, &u32_val);
-	ok1(u32_val == bmsg_decode_uint32(bytes));
-	ok1(bmsg_parser_uint64_done(&u64_prsr) != 0);
-	bmsg_parser_uint64_value(&u64_prsr, &u64_val);
-	ok1(u64_val == bmsg_decode_uint64(bytes));
+		diag("input = 0x%016" PRIx64, input);
+		i = 0;
+		diag("i = %zu", i);
+		bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
+		bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
+		bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
+		ok1(bmsg_parser_uint16_done(&u16_prsr) == 0);
+		ok1(bmsg_parser_uint32_done(&u32_prsr) == 0);
+		ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
+	
+		i++;
+		diag("i = %zu", i);
+		bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
+		bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
+		bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
+		ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
+		bmsg_parser_uint16_value(&u16_prsr, &u16_val);
+		ok1(u16_val == bmsg_decode_uint16(bytes));
+		ok1(bmsg_parser_uint32_done(&u32_prsr) == 0);
+		ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
+	
+		i++;
+		diag("i = %zu", i);
+		bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
+		bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
+		bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
+		ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
+		bmsg_parser_uint16_value(&u16_prsr, &u16_val);
+		ok1(u16_val == bmsg_decode_uint16(bytes));
+		ok1(bmsg_parser_uint32_done(&u32_prsr) == 0);
+		ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
+		
+		i++;
+		diag("i = %zu", i);
+		bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
+		bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
+		bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
+		ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
+		bmsg_parser_uint16_value(&u16_prsr, &u16_val);
+		ok1(u16_val == bmsg_decode_uint16(bytes));
+		ok1(bmsg_parser_uint32_done(&u32_prsr) != 0);
+		bmsg_parser_uint32_value(&u32_prsr, &u32_val);
+		ok1(u32_val == bmsg_decode_uint32(bytes));
+		ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
+	
+		i++;
+		diag("i = %zu", i);
+		bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
+		bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
+		bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
+		ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
+		bmsg_parser_uint16_value(&u16_prsr, &u16_val);
+		ok1(u16_val == bmsg_decode_uint16(bytes));
+		ok1(bmsg_parser_uint32_done(&u32_prsr) != 0);
+		bmsg_parser_uint32_value(&u32_prsr, &u32_val);
+		ok1(u32_val == bmsg_decode_uint32(bytes));
+		ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
+	
+		i++;
+		diag("i = %zu", i);
+		bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
+		bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
+		bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
+		ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
+		bmsg_parser_uint16_value(&u16_prsr, &u16_val);
+		ok1(u16_val == bmsg_decode_uint16(bytes));
+		ok1(bmsg_parser_uint32_done(&u32_prsr) != 0);
+		bmsg_parser_uint32_value(&u32_prsr, &u32_val);
+		ok1(u32_val == bmsg_decode_uint32(bytes));
+		ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
+	
+		i++;
+		diag("i = %zu", i);
+		bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
+		bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
+		bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
+		ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
+		bmsg_parser_uint16_value(&u16_prsr, &u16_val);
+		ok1(u16_val == bmsg_decode_uint16(bytes));
+		ok1(bmsg_parser_uint32_done(&u32_prsr) != 0);
+		bmsg_parser_uint32_value(&u32_prsr, &u32_val);
+		ok1(u32_val == bmsg_decode_uint32(bytes));
+		ok1(bmsg_parser_uint64_done(&u64_prsr) == 0);
+	
+		i++;
+		diag("i = %zu", i);
+		bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
+		bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
+		bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
+		ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
+		bmsg_parser_uint16_value(&u16_prsr, &u16_val);
+		ok1(u16_val == bmsg_decode_uint16(bytes));
+		ok1(bmsg_parser_uint32_done(&u32_prsr) != 0);
+		bmsg_parser_uint32_value(&u32_prsr, &u32_val);
+		ok1(u32_val == bmsg_decode_uint32(bytes));
+		ok1(bmsg_parser_uint64_done(&u64_prsr) != 0);
+		bmsg_parser_uint64_value(&u64_prsr, &u64_val);
+		ok1(u64_val == bmsg_decode_uint64(bytes));
+	
+		i++;
+		diag("i = %zu", i);
+		bmsg_parser_uint16_consume(&u16_prsr, bytes[i]);
+		bmsg_parser_uint32_consume(&u32_prsr, bytes[i]);
+		bmsg_parser_uint64_consume(&u64_prsr, bytes[i]);
+		ok1(bmsg_parser_uint16_done(&u16_prsr) != 0);
+		bmsg_parser_uint16_value(&u16_prsr, &u16_val);
+		ok1(u16_val == bmsg_decode_uint16(bytes));
+		ok1(bmsg_parser_uint32_done(&u32_prsr) != 0);
+		bmsg_parser_uint32_value(&u32_prsr, &u32_val);
+		ok1(u32_val == bmsg_decode_uint32(bytes));
+		ok1(bmsg_parser_uint64_done(&u64_prsr) != 0);
+		bmsg_parser_uint64_value(&u64_prsr, &u64_val);
+		ok1(u64_val == bmsg_decode_uint64(bytes));
+		
+	}
 
 	diag("----parser/integer----\n#");
 
